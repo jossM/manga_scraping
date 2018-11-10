228c299a367f014d937dfdcc584e803e8deeb7d0
josselin-perso <josselin.mahe@gmail.com>
2018-11-10 21:47:32 +0100

    feat: handle - in chapter

--- lambda_function.py
+++ lambda_function.py
@@ -4,11 +4,12 @@ from typing import Iterator
 import warnings
 import queue
 
-from dynamo.page_marks_db import PageMark
-from skraper import scrap_bakaupdate, ScrappedReleases
+from dynamo import page_marks_db
+import skraper
+import web_search
 
 
-def to_yielder(q: queue.Queue, max_number_result: int, timeout: int) -> Iterator[ScrappedReleases]:
+def to_yielder(q: queue.Queue, max_number_result: int, timeout: int) -> Iterator[skraper.ScrappedReleases]:
     for _ in range(max_number_result):
         try:
             yield q.get(block=True, timeout=timeout)
@@ -21,10 +22,10 @@ def handler_scheduled_scraping(event, context):
 
     # scraping
     with warnings.catch_warnings(record=True) as triggered_warning:
-        page_marks = PageMark.get_all()
+        page_marks = page_marks_db.PageMark.get_all()
         result_queue = queue.Queue()
         with closing(pool.Pool(cpu_count()-1)) as request_pool:
-            request_pool.map(scrap_bakaupdate, [(result_queue, page_mark.serie_id) for page_mark in page_marks])
+            request_pool.map(skraper.scrap_bakaupdate, [(result_queue, page_mark) for page_mark in page_marks])
         mail_message.add_warnings(triggered_warning)
 
     # compering to reading state
@@ -34,10 +35,13 @@ def handler_scheduled_scraping(event, context):
         if not scrapped_releases.releases:
             continue
         previous_page_marks = page_marks_map[scrapped_releases.serie_id]
-        mail_message.add_releases(
-            serie_name=previous_page_marks.serie_name,
-            releases=[complete_release_info(release) for release in scrapped_releases.releases
-                      if release not in previous_page_marks.chapter_marks])  # todo: add hashing to chapter
+        new_releases = [release for release in page_marks_map[scrapped_releases.serie_id].releases
+                        if release not in previous_page_marks.chapter_marks]
+        if not new_releases:
+            continue
+        new_releases_with_link = [web_search.add_likely_link(previous_page_marks.serie_name, release)
+                                  for release in new_releases]
+        mail_message.add_releases(serie_name=previous_page_marks.serie_name, releases=new_releases_with_link)
     # send mail
-    mail_str = mail_message.build()
-    send_to_sns(mail_str)
+    html_mail_str = mail_message.make_html()
+    send_to_sns(html_mail_str)
--- skraper.py
+++ skraper.py
@@ -1,11 +1,12 @@
-from typing import Iterable, List, Union
+from typing import Iterable, List
 from queue import Queue
 import warnings
 
 from bs4 import BeautifulSoup
 import requests
 
-from chapter_type import Chapter
+import chapter_type
+from dynamo import page_marks_db
 
 
 class ScrappingWarning(UserWarning):
@@ -15,7 +16,7 @@ class ScrappingWarning(UserWarning):
 class ScrappedReleases(object):
     """ represents data returned from scrapping """
 
-    class ScrappedChapterRelease(Chapter):
+    class ScrappedChapterRelease(chapter_type.Chapter):
         def __init__(self, chapter: str, group: str):
             super(ScrappedReleases.ScrappedChapterRelease, self).__init__(chapter)
             self.group = group
@@ -24,13 +25,27 @@ class ScrappedReleases(object):
         def chapter(self):
             return self._chapter
 
-    def __init__(self, serie_id: str, chapters_releases: Iterable[ScrappedChapterRelease], warning_message=None):
+    def __init__(self,
+                 serie_id: str,
+                 chapters_releases: Iterable[ScrappedChapterRelease],
+                 warning_message=None,
+                 observed_diff_between_chapter: float=None,
+                 deduced: bool=False):
         self.serie_id = serie_id
-        self.releases = sorted(chapters_releases, reverse=True)
+        self.releases = sorted(chapters_releases)
         self.warning_message = warning_message
+        if not self.releases:
+            if not observed_diff_between_chapter:
+                self.min_dif_between_chapter = 1.
+            else:
+                self.min_dif_between_chapter = observed_diff_between_chapter
+        else:
+            releases_diff = [self.releases[i + 1] - self.releases[i] for i in range(len(self.releases)-1)]
+            self.min_dif_between_chapter = \
+                min([observed_diff_between_chapter] + [diff for diff in releases_diff if diff])
+        self.deduced = deduced
 
-    @property
-    def warning(self):
+    def has_warning(self):
         return self.warning_message is not None
 
     def __repr__(self):
@@ -57,33 +72,37 @@ def _extract_soup_data(soup: BeautifulSoup, tag: str) -> List[str]:
     return result
 
 
-def scrap_bakaupdate(result_queue: Queue, serie_id: str) -> None:
+def scrap_bakaupdate(result_queue: Queue, serie_page_mark: page_marks_db.PageMark) -> None:
     """
     Scraps data from bakaupdate for releases.
     Function to be executed in a thread to avoid waiting.
     """
-    bakaupdate_page = requests.get(f'https://www.mangaupdates.com/series.html?id={serie_id}')
+    bakaupdate_page = requests.get(f'https://www.mangaupdates.com/series.html?id={serie_page_mark.serie_id}')
     bakaupdate_soup = BeautifulSoup(bakaupdate_page.content, features="lxml")
     latest_release_title_soup = bakaupdate_soup.find(string='Latest Release(s)').parent.parent
     latest_release_div_soup = latest_release_title_soup.find_next_sibling()
     chapters = _extract_soup_data(latest_release_div_soup, 'i')
     scanlation_groups = _extract_soup_data(latest_release_div_soup, 'a')
-    warning_message = None
+    warning_message = ''
     if len(chapters) > len(scanlation_groups):
         warning_message = f'For some strange reason, {len(chapters) - len(scanlation_groups)} more chapters have been '\
                           f'scraped than for groups'
     elif len(chapters) < len(scanlation_groups):
         warning_message = f'For some strange reason, {len(chapters) - len(scanlation_groups)} more groups have been ' \
                           f'scraped than for chapters'
-    if warning_message:
-        warning_message += f'</br>For serie : {serie_id}: display might be wrong. original display was : </br>' \
-                           f'<div>{latest_release_div_soup.prettify()}</div></br>'
-        warnings.warn(warning_message,ScrappingWarning)
 
     max_index = max(len(chapters), len(scanlation_groups))
-    result_queue.put(
-        ScrappedReleases(
-            serie_id=serie_id,
-            chapters_releases=[ScrappedReleases.ScrappedChapterRelease(chapter, group)
-                               for chapter, group in zip(chapters[:max_index], scanlation_groups[:max_index])],
-            warning_message=warning_message))
+    chapter_release_info = list(zip(chapters[:max_index], scanlation_groups[:max_index]))
+    chapter_release_info.sort(key=lambda zip_chapter: '-' in str(zip_chapter[0]))
+
+    scrapped_chapters_releases = []
+    for chapter, group in chapter_release_info:
+        for split_chapter in chapter.split('-'):
+            scrapped_chapters_releases.append(ScrappedReleases.ScrappedChapterRelease(split_chapter, group))
+
+    if warning_message:
+        warning_message += \
+            f'</br>For serie : {serie_page_mark.serie_id}: display might be wrong. original display was : ' \
+            f'</br><div>{latest_release_div_soup.prettify()}</div>'
+        warnings.warn(warning_message, ScrappingWarning)
+    result_queue.put(ScrappedReleases(serie_page_mark.serie_id, scrapped_chapters_releases, warning_message))
