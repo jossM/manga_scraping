9b9dfc4a96a85cc3cf44b2a52d5d174a48062d56
jossM <josselin.mahe@gmail.com>
2018-12-30 10:12:13 +0100

    add todo comment for MEP

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def handle_scheduled_scraping(event, context):
             logger.error(f'For some strange reason serie {releases.serie_id} is present but absent from releases.')
             continue
         page_marks_map[releases.serie_id].extend(releases.releases)
-    # page_marks_db.batch_put(page_marks)
+    # page_marks_db.batch_put(page_marks) # todo: remove comment here
 
 
 if __name__ == "__main__":
